03c44d012096dc93c5c20f2b2a2be0959a108485
Marat1nf <146007138+Marat1nf@users.noreply.github.com>
2023-09-29 20:04:27 +0300

    Work from youtube

---
+++ index.js
@@ -0,0 +1,24 @@
+// Variables
+let substract = document.querySelector("#substract");
+let add = document.querySelector("#add");
+
+console.log(substract);
+
+substract.addEventListener("click", function () {
+  //Substract value (-1)
+  let output = document.querySelector("#result");
+  let result = Number(output.innerHTML) - 1;
+  if (result >= 0) {
+    output.innerHTML = result;
+  }
+  console.log(output);
+});
+
+add.addEventListener("click", function () {
+  //Add value (+1)
+  let output = document.querySelector("#result");
+  let result = Number(output.innerHTML) + 1;
+
+  output.innerHTML = result;
+  console.log(output);
+});
